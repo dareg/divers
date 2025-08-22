a8fd5e4120c384ef4622fd3425bc2c1c1cc04b00
Judicaël Grasset <judicael@posteo.de>
2025-08-22 21:05:26 +0200

    améliore la gestion du volume dans i3
    Parfois la commande d'augmentation était lancé une infinité de fois et explosait les oreilles et celles des voisins

---
+++ scripts/augmente_volume.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+safe_volume_up() {
+    local increment=${1:-5}
+    local current_vol=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+(?=%)' | head -1)
+    local new_vol=$((current_vol + increment))
+    
+    if [ $new_vol -le 100 ]; then
+        pactl set-sink-volume @DEFAULT_SINK@ ${new_vol}%
+    else
+        pactl set-sink-volume @DEFAULT_SINK@ 100%
+    fi
+}
+
+safe_volume_up 5
+
